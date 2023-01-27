07b8ac624d1f854cc8158bbf77b5377831c4a369
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-27 16:58:36 -0500

    Initial commit

---
+++ src/tests/test_hyundai.py
@@ -0,0 +1,75 @@
+import pytest
+from faker import Faker
+from fastapi.testclient import TestClient
+
+from src.tests.test_helpers import program_vcr
+from src.main import app
+
+client = TestClient(app)
+vcr = program_vcr()
+
+
+@pytest.fixture(name="test_cassette", params=["Ioniq%205"])
+def _test_cassette(request):
+    fake = Faker()
+    vehicle_model = request.param
+
+    params = {"zip": "00501", "year": "2023", "radius": "125", "model": vehicle_model}
+    headers = {"User-Agent": fake.user_agent()}
+
+    cassette_name = f"hyundai-{vehicle_model}.yaml"
+    with vcr.use_cassette(cassette_name):
+        r = client.get("/api/inventory/hyundai", headers=headers, params=params)
+
+    return r
+
+
+def test_api_returns_200(test_cassette):
+    assert test_cassette.status_code == 200, (
+        f"API response status code was "
+        f"{test_cassette.status_code}, it was expected to be 200"
+    )
+
+
+def test_hyundai_inventory_response_is_json(test_cassette):
+    try:
+        test_cassette.json()
+    except Exception:
+        pytest.fail(f"API response is not valid JSON. It was: {test_cassette.text}")
+
+
+def test_hyundai_inventory_response_is_a_success(test_cassette):
+    """The Hyundai API thinks our request was a success"""
+    assert (
+        test_cassette.json()["status"] == "SUCCESS"
+    ), "'SUCCESS' was not found in the Hyundai Inventory response"
+
+
+def test_hyundai_inventory_has_dealers(test_cassette):
+    """Dealers with inventory are returned"""
+    assert (
+        len(test_cassette.json()["data"][0]["dealerInfo"]) >= 1
+    ), "API response was a Success but no dealers have inventory"
+
+
+def test_hyundai_inventory_dealer_has_inventory(test_cassette):
+    """Dealers have inventory"""
+    assert (
+        len(test_cassette.json()["data"][0]["dealerInfo"][0]["vehicles"]) >= 1
+    ), "API response was a Success but no dealers have inventory"
+
+
+# def test_get_vin_detail():
+#     pass
+
+
+# def test_get_inventory_no_results():
+#     pass
+
+
+# def test_get_inventory_produces_error():
+#     pass
+
+
+# def test_get_vin_detail_produces_error():
+#     pass
---
+++ src/tests/test_query_param_validation.py
@@ -0,0 +1,127 @@
+import pytest
+from fastapi import Depends
+from fastapi.testclient import TestClient
+
+from src.libs.common_query_params import CommonInventoryQueryParams
+from src.tests.test_helpers import generate_test_query_params
+from src.main import app
+
+client = TestClient(app)
+
+
+@app.get("/api/inventory/tests")
+def foo(req_params: CommonInventoryQueryParams = Depends()) -> dict:
+    """This endpoint is used for query parameter validation testing"""
+    return {}
+
+
+def test_non_existent_route():
+    r = client.get("/nothing-to-see-here")
+    assert r.status_code == 404
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_status_code, assertion_message",
+    [
+        ("2022", 200, "Valid year"),
+        ("2023", 200, "Valid year"),
+        ("2021", 422, "Invalid year"),
+        ("201", 422, "Invalid year, too short"),
+        ("-2022", 422, "Invalid year, this is a negative value"),
+        ("notayear", 422, "Invalid year, this is a string"),
+        ("🤔", 422, "Invalid year, this is an emoji"),
+    ],
+)
+def test_year_validation(test_input, expected_status_code, assertion_message):
+    test_params = generate_test_query_params()
+    test_params["year"] = test_input
+    r = client.get("/api/inventory/tests", params=test_params)
+
+    assert r.status_code == expected_status_code, assertion_message
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_status_code, assertion_message",
+    [
+        # TODO:
+        # Need to rethink validation for this case. The existing validations won't catch
+        # numbers of len() < 5 where the number is > 500
+        # ("1234", 422, "Zip not complete, too short"),
+        ("notanumber", 422, "Zip should be a number"),
+        ("🤔", 422, "Zip codes can't be emoji...yet"),
+        ("00500", 422, "Zip code not valid (too low)"),
+        ("99951", 422, "Zip code not valid (too high)"),
+        ("0", 422, "Zip code not valid (too low, too short)"),
+        ("-12345", 422, "Not a valid zip code"),
+        ("98585-5002", 422, "Not a 5-digit zip code"),
+        ("12345", 200, "Valid zip"),
+        ("00501", 200, "Valid first zip code"),
+        ("99950", 200, "Valid last zip code"),
+    ],
+)
+def test_zip_validation(test_input, expected_status_code, assertion_message):
+    test_params = generate_test_query_params()
+    test_params["zip"] = test_input
+    r = client.get("/api/inventory/tests", params=test_params)
+
+    assert r.status_code == expected_status_code, assertion_message
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_status_code, assertion_message",
+    [
+        ("0", 422, "Radius too small"),
+        ("-1", 422, "Not a valid radius"),
+        ("🤔", 422, "Not a valid radius"),
+        ("1000", 422, "Radius too large"),
+        ("1", 200, "Minimum radius"),
+        ("999", 200, "Maximum radius"),
+    ],
+)
+def test_radius_validation(test_input, expected_status_code, assertion_message):
+    test_params = generate_test_query_params()
+    test_params["radius"] = test_input
+    r = client.get("/api/inventory/tests", params=test_params)
+
+    assert r.status_code == expected_status_code, assertion_message
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_status_code, assertion_message",
+    [
+        ("Z", 422, "Is not a valid model"),
+        ("Ioniq 8", 422, "Is not a valid model"),
+        ("Fahrzeug", 422, "Is not a valid model"),
+        ("автомобіль", 422, "Is not a valid model"),
+        ("12345", 422, "Is not a valid model"),
+        ("Ioniq%205", 200, "Is a valid model"),
+        ("Ioniq 5", 200, "Is a valid model"),
+        ("Ioniq+5", 200, "Is a valid model"),
+        ("Ioniq%206", 200, "Is a valid model"),
+        ("Ioniq%20Phev", 200, "Is a valid model"),
+        ("Kona%20Ev", 200, "Is a valid model"),
+        ("Santa%20Fe%20Phev", 200, "Is a valid model"),
+        ("Tucson%20Phev", 200, "Is a valid model"),
+        ("N", 200, "Is a valid model"),
+        ("V", 200, "Is a valid model"),
+        ("F", 200, "Is a valid model"),
+        ("R", 200, "Is a valid model"),
+        ("T", 200, "Is a valid model"),
+        ("GV60", 200, "Is a valid model"),
+        ("ElectrifiedG80", 200, "Is a valid model"),
+        ("ID.4", 200, "Is a valid model"),
+        ("mache", 200, "Is a valid model"),
+        ("Bolt EV", 200, "Is a valid model"),
+        ("Bolt EUV", 200, "Is a valid model"),
+        ("etron", 200, "Is a valid model"),
+        ("etrongt", 200, "Is a valid model"),
+        ("q4", 200, "Is a valid model"),
+    ],
+)
+def test_model_validation(test_input, expected_status_code, assertion_message):
+    test_params = generate_test_query_params()
+    test_params["model"] = test_input
+
+    r = client.get("/api/inventory/tests", params=test_params)
+
+    assert r.status_code == expected_status_code, assertion_message
