c76461af8885b21dcb847dff160b5a61ced66841
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-26 21:50:48 -0500

    Migration of the Hyundai API

--- src/routers/hyundai.py
+++ src/routers/hyundai.py
@@ -1,10 +1,57 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+
 from libs.common_query_params import CommonInventoryQueryParams
+from libs.exceptions import error_response
+from libs.http import AsyncHTTPClient, send_response
 
 router = APIRouter(prefix="/api")
 
 
 @router.get("/inventory/hyundai")
-async def testing(params: CommonInventoryQueryParams = Depends()):
+async def testing(
+    req: Request, req_params: CommonInventoryQueryParams = Depends()
+) -> dict:
+    # api_url = (
+    #     "https://www.hyundaiusa.com/var/hyundai/services/inventory/vehicleList.json"
+    # )
+    params = {
+        "zip": req_params.zip,
+        "year": req_params.year,
+        "model": "Ioniq%205",
+        "radius": req_params.radius,
+    }
+    headers = {
+        "User-Agent": req.headers.get("User-Agent"),
+        "referer": "https://www.hyundaiusa.com/us/en/vehicles",
+    }
 
-    return params.zip, params.year
+    # Make a call to the Hyundai API
+    async with AsyncHTTPClient(
+        base_url="https://www.hyundaiusa.com", timeout_value=30
+    ) as http:
+        g = await http.get(
+            uri="/var/hyundai/services/inventory/vehicleList.json",
+            headers=headers,
+            params=params,
+        )
+        data = g.json()
+
+        # Ensure the response back from the API has some status, indicating a successful
+        # API call
+        try:
+            data["status"]
+        except KeyError:
+            return error_response(
+                error_message="Invalid data received from the Hyundai API",
+                error_data=data,
+                status_code=500,
+            )
+
+        if "SUCCESS" in data["status"]:
+            return send_response(response_data=data)
+        else:
+            return error_response(
+                error_message="Received invalid data from the Hyundai API",
+                error_data=data,
+                status_code=400,
+            )
