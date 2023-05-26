9ac6af95466d502e58262087a614b20c63a07f9e
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-05-26 13:53:25 -0400

    Removing dead code

--- src/libs/libs.py
+++ src/libs/libs.py
@@ -1,114 +1,11 @@
 import asyncio
-import json
 import time
-
 from functools import partial, wraps
-
-
-def validate_request(input_data):
-    is_valid_request = True
-    for k, v in input_data:
-        if not validate(k, v):
-            is_valid_request = False
-            print(f"\n\nFailed to validate: {k} {v}")
-            break
-
-    return is_valid_request
-
-
-def validate(validation_type, validation_data):
-    # https://facts.usps.com/42000-zip-codes/
-    valid_zip_codes = [501, 99950]  # Starting zip code is 00501
-    valid_years = ["2022", "2023"]
-    valid_models = [
-        "Ioniq%205",
-        "Ioniq%206",
-        "Ioniq%20Phev",
-        "Kona%20Ev",
-        "Santa%20Fe%20Phev",
-        "Tucson%20Phev",
-        "N",  # EV6
-        "V",  # Niro EV
-        "F",  # Niro Plug-in Hybrid
-        "R",  # Sportage Plug-in Hybrid
-        "T",  # Sorento Plug-in Hybrid
-        "GV60",  # Genesis GV60
-        "ElectrifiedG80",  # Genesis Electrified G80
-        "ID.4",  # VW ID.4
-        "mache",  # Ford Mustang Mach-E
-        "Bolt EV",  # Chevrolet Bolt EV
-        "Bolt EUV",  # Chevrolet Bolt EUV
-        "etron",  # Audi e-tron
-        "etrongt",  # Audi e-tron GT
-        "q4",  # Audi Q4 e-tron
-    ]
-    valid_radii = [1, 999]
-
-    if "zip" in validation_type:  # Capture variations of "zip code"
-        # Can zip be cast to an int?
-        try:
-            int(validation_data)
-        except ValueError:
-            return False
-
-        # Zip too short
-        if len(validation_data) != 5:
-            return False
-
-        # Is zip within valid zip codes?
-        return (
-            int(validation_data) >= valid_zip_codes[0]
-            and int(validation_data) <= valid_zip_codes[1]
-        )
-
-    elif validation_type == "year":
-        return validation_data in valid_years
-
-    elif validation_type == "model" or validation_type == "series":
-        return validation_data in valid_models
-
-    elif validation_type == "radius":
-        # Can radius be cast to an int?
-        try:
-            int(validation_data)
-        except ValueError:
-            return False
-
-        # Is radius within valid_radii?
-        return (
-            int(validation_data) >= valid_radii[0]
-            and int(validation_data) <= valid_radii[1]
-        )
-
-
-# TODO: Move this logic to the Vue app
-def flatten_api_results(input_data: str):
-    tmp = []
-    input_data = input_data["data"][0]["dealerInfo"]
-
-    if input_data:
-        for dealer in range(0, len(input_data)):
-            for k, v in input_data[dealer].items():
-                if "vehicles" in k and v is not None:
-                    for i in range(0, len(v)):
-                        tmp.append(
-                            {**input_data[dealer], **input_data[dealer]["vehicles"][i]}
-                        )
-
-        for i in range(0, len(tmp)):
-            del tmp[i]["vehicles"]
-
-            tmp[i]["ExtColorLongDesc"] = tmp[i]["colors"][0]["ExtColorLongDesc"]
-
-        return json.dumps(tmp)
-    else:
-        return json.dumps({})
 
 
 def timeit(function_to_time):
     @wraps(function_to_time)
     def timed(*args, **kw):
-
         start_time = time.perf_counter_ns()
         output = function_to_time(*args, **kw)
         end_time = time.perf_counter_ns()
