8bb2c6631437f2089a12c580204d120820b28345
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-07-30 10:30:56 -0400

    Removing debug logging

--- src/libs/responses.py
+++ src/libs/responses.py
@@ -18,8 +18,6 @@ def error_response(
     error_message: str, error_data: dict | None = None, status_code: int = 500
 ):
     """Return a standardized error response to the caller."""
-    # Logging this error
-    # logging.warning(f"{error_message}: {error_data}")
 
     raise HTTPException(
         status_code=status_code,
