293aae660897706f9e0f7a13212d59744aef6a54
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-08-10 11:31:11 -0400

    Adding httpstat.us helper

--- src/routers/helpers.py
+++ src/routers/helpers.py
@@ -3,6 +3,7 @@ import os
 from fastapi import APIRouter, Path
 
 from src.libs.responses import error_response, send_response
+from src.libs.http import AsyncHTTPClient
 
 router = APIRouter(prefix="/api")
 verify_ssl = True
@@ -29,3 +30,31 @@ def send_error_response(
     return error_response(
         error_message=f"This is a {status_code} error", status_code=status_code
     )
+
+
+@router.get("/test/status/{status_code}")
+async def send_httpstatus_error_response(
+    status_code: int = Path(
+        title="A HTTP status code in the 400 or 500 class", ge=400, le=599
+    ),
+):
+    async with AsyncHTTPClient(
+        base_url="https://httpstat.us", timeout_value=1.0, verify=True
+    ) as http:
+        params = {"sleep": 2000}
+        headers = {"User-Agent": "Test"}
+        h = await http.get(uri=f"/{status_code}", params=params, headers=headers)
+
+    try:
+        h.json()
+        return error_response(
+            error_message=f"This is a error: {h}", status_code=status_code
+        )
+    except AttributeError:
+        return error_response(
+            error_message=f"This is a error: {h}", status_code=status_code
+        )
+
+    # return "foo"
+    # return h.text if h.text else "foo"
+    #
