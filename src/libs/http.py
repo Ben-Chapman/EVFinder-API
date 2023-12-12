d522e72d7f8fa9ced1506d7ada09abba057af541
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2023-12-11 21:32:30 -0500

    Adding support for the 2024 Audi Q8 e-tron and SQ8 e-tron (#16)
    * Add support for the 2024 Audi Q8
    
    * Add support for the 2024 Audi SQ8 e-tron
    
    * Refactor to support asyncio for HTTP POST requests
    
    * Refactor to support parallel asyncio calls to the Audi API
    
    * Support async API paging

--- src/libs/http.py
+++ src/libs/http.py
@@ -1,8 +1,9 @@
 import asyncio
 import time
+from typing import Literal
 
 import httpx
-from src.libs.libs import async_timeit
+
 from src.libs.responses import error_response
 from src.routers.logger import send_error_to_gcp
 
@@ -15,7 +16,7 @@ class AsyncHTTPClient:
         use_http2: bool = True,
         verify: bool = True,
     ):
-        """A HTTP helper library to be used to fetch manufacturer inventory. This library
+        """A HTTP helper library used to fetch manufacturer inventory. This library
         is designed to be called directly or used as a context manager and is async
         compatible.
 
@@ -48,9 +49,7 @@ class AsyncHTTPClient:
 
     async def __aexit__(self, exception_type, exception_value, traceback):
         end_time = time.perf_counter()
-        print(
-            f"Transaction for {self.base_url} took {end_time - self.start_time} seconds."
-        )
+        print(f"{end_time - self.start_time} sec - {self.base_url}")
         await self.client.aclose()
 
     async def close(self):
@@ -85,15 +84,72 @@ class AsyncHTTPClient:
 
         if loop and loop.is_running():
             result = loop.create_task(
-                self.gather_urls_for_asyncio(uri, headers, params)
+                self.gather_urls_for_asyncio(uri, headers, params, method="get")
             )
         else:
             # If for some reason we don't have a running event loop, start one
-            result = asyncio.run(self.gather_urls_for_asyncio(uri, headers, params))
+            result = asyncio.run(
+                self.gather_urls_for_asyncio(uri, headers, params, method="get")
+            )
 
         results = await result
 
-        # If the length of the results list is 1, this is likely the results of a single
+        # If the length of the results list is 1, this is likely the result of a single
+        # HTTP request. So returning just that done result, not a list containing one item.
+        # If the length is > 1, return the list containing the httpx.Responses.
+        # TODO: Return all results as a list. Will need to refactor all manufacturer
+        # routers.
+        if len(results) == 1:
+            return results[0]
+        else:
+            return results
+
+    async def post(
+        self,
+        uri: str | list,
+        headers: dict | None = None,
+        post_data: dict | None = None,
+    ) -> list | httpx.Response:
+        """Wrap the asyncio coroutine into a Task and schedule its execution.
+
+        Args:
+            uri (str | list): A single HTTP URI, or list of HTTP URIs to fetch. If uri
+            is a list, it must be in the format of: [uri, headers, params].
+
+            headers (dict | None, optional): HTTP headers to include with this request.
+            Defaults to None.
+
+            post_data (dict | None, optional): HTTP POST data to include with this request.
+            Defaults to None.
+
+        Returns:
+            (list | httpx.Response): The result of an asyncio Task object.
+            If multiple URLs were fetched, returns a list of httpx.Responses. If a single
+            URL was fetched returns a single httpx.Response.
+        """
+        # Make sure we have a running event loop
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:  # 'RuntimeError: There is no current event loop...'
+            loop = None
+
+        if loop and loop.is_running():
+            result = loop.create_task(
+                self.gather_urls_for_asyncio(
+                    uri=uri, headers=headers, params=post_data, method="post"
+                )
+            )
+        else:
+            # If for some reason we don't have a running event loop, start one
+            result = asyncio.run(
+                self.gather_urls_for_asyncio(
+                    uri=uri, headers=headers, params=post_data, method="post"
+                )
+            )
+
+        results = await result
+
+        # If the length of the results list is 1, this is likely the result of a single
         # HTTP request. So returning just that one result, not a list containing one item.
         # If the length is > 1, return the list containing the httpx.Responses.
         # TODO: Return all results as a list. Will need to refactor all manufacturer
@@ -104,7 +160,11 @@ class AsyncHTTPClient:
             return results
 
     async def gather_urls_for_asyncio(
-        self, uri: str | list, headers: dict | None = None, params: dict | None = None
+        self,
+        uri: str | list,
+        headers: dict | None = None,
+        params: dict | None = None,
+        method: Literal["get", "post"] = "get",
     ):
         """Create a list of tasks required for asyncio to run. These tasks are httpx
         request futures.
@@ -116,45 +176,67 @@ class AsyncHTTPClient:
             headers (dict | None, optional): HTTP headers to include with this request.
             Defaults to None.
 
-            params (dict | None, optional): HTTP query parameters to include with this request.
+            params (dict | None, optional): HTTP query parameters or an HTTP POST body
+            to include with this request.
             Defaults to None.
+
+            method (str): Which HTTP method to use for this request. get and post are accepted.
 
         Returns:
             list: An aggregate list of returned values from the HTTPX request.
         """
         tasks = []
 
+        # If we have a single URL to fetch
         if type(uri) is str:
             if headers or params:
                 tasks.append(
-                    self.fetch_api_data(uri=uri, headers=headers, params=params)
+                    self.fetch_api_data(
+                        uri=uri, headers=headers, params=params, method=method
+                    )
                 )
         else:
+            # If we have multiple URLs to fetch. Add each to the task queue
             for url in uri:
                 tasks.append(
-                    self.fetch_api_data(uri=url[0], headers=url[1], params=url[2])
+                    self.fetch_api_data(
+                        uri=url[0], headers=url[1], params=url[2], method=method
+                    )
                 )
 
         return await asyncio.gather(*tasks)
 
     async def fetch_api_data(
-        self, uri: str, headers: dict, params: dict | None = None
+        self,
+        uri: str,
+        headers: dict,
+        params: dict | None = None,
+        method: Literal["get", "post"] = "get",
     ) -> dict:
         """Helper function to issue API requests through HTTPX
 
         Args:
             uri (str): A HTTP URI to fetch
+
             headers (dict): HTTP headers to include with this request.
-            params (dict | None, optional): HTTP query parameters to include with this request.
-            Defaults to None.
+
+            params (dict | None, optional): HTTP query parameters or an HTTP POST body
+            to include with this request.
+            Defaults to None
+
+            method (str): Which HTTP method to use for this request. get and post are accepted.
 
         Returns:
             ResponseObject: The HTTP response from the requested URL.
         """
+
         error_message = "An error occurred obtaining vehicle inventory for this search."
 
         try:
-            resp = await self.client.get(uri, headers=headers, params=params)
+            if method == "get":
+                resp = await self.client.get(url=uri, headers=headers, params=params)
+            elif method == "post":
+                resp = await self.client.post(url=uri, headers=headers, json=params)
             resp.raise_for_status()
 
         except (httpx.TimeoutException, httpx.ReadTimeout) as e:
@@ -219,97 +301,6 @@ class AsyncHTTPClient:
                     "url": str(e.request.url),
                     "user_agent": headers.get("User-Agent"),
                     "status_code": "400",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        # Base exceptions to catch all remaining errors
-        except httpx.HTTPStatusError as e:
-            error_data = f"""Error response {e.response.status_code} while requesting
-            {e.request.url!r}."""
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "500",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        except httpx.RequestError as e:
-            error_data = f"An error occurred while requesting {e.request.url!r}. {e}"
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "400",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        else:
-            return resp
-
-    @async_timeit
-    async def post(self, uri: str, headers: dict, post_data: dict):
-        error_message = "An error occurred obtaining vehicle inventory for this search."
-        try:
-            resp = await self.client.post(uri, headers=headers, json=post_data)
-            resp.raise_for_status()
-
-        except (httpx.TimeoutException, httpx.ReadTimeout) as e:
-            error_data = f"The request to {e.request.url!r} timed out."
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "504",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        except httpx.NetworkError as e:
-            error_data = f"A network error occurred for {e.request.url!r}. {e}."
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "503",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        except httpx.DecodingError as e:
-            error_data = f"""Decoding of the response to {e.request.url!r} failed,
-                due to a malformed encoding. {e}."""
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "400",
-                },
-            )
-            return error_response(error_message=error_message, error_data=error_data)
-
-        except httpx.TooManyRedirects as e:
-            error_data = f"Too many redirects for {e.request.url!r} failed. {e}."
-            send_error_to_gcp(
-                error_message,
-                http_context={
-                    "method": e.request.method,
-                    "url": str(e.request.url),
-                    "user_agent": headers.get("User-Agent"),
-                    "status_code": "429",
                 },
             )
             return error_response(error_message=error_message, error_data=error_data)
--- src/routers/audi.py
+++ src/routers/audi.py
@@ -1,3 +1,4 @@
+import copy
 from fastapi import APIRouter, Depends, Request
 
 from src.libs.common_query_params import CommonInventoryQueryParams
@@ -23,6 +24,9 @@ async def get_audi_inventory(
         "Referer": "https://www.audiusa.com/",
     }
 
+    amount_to_page_by = 24
+    offset = 0
+
     inventory_post_data = {
         "operationName": "getFilteredVehiclesForWormwood",
         "variables": {
@@ -33,41 +37,81 @@ async def get_audi_inventory(
                 "available-from.immediately,"
                 "available-from.soon,"
                 f"geo:{geo}_{radius}_miles_defaultcity,"
-                f"model-group-range.{model},"
+                f"model-range.{model},"
                 "vtp-drivetrain.electrical,"
                 f"model-year.{year}"
             ),
             "sort": "byDistance:ASC",
-            "limit": 1000,
-            "offset": 0,
+            "limit": amount_to_page_by,
+            "offset": offset,
             "preset": "foreign-brand.no,sold-order.no",
         },
         "query": "query getFilteredVehiclesForWormwood($version: String, $market: [MarketType]!, $limit: Int, $lang: String!, $filters: String, $sort: String, $offset: Int, $preset: String) { getFilteredVehiclesForWormwood( version: $version market: $market size: $limit lang: $lang filters: $filters sort: $sort from: $offset preset: $preset ) { filterResults { totalCount totalNewCarCount totalUsedCarCount available_from_soon available_from_immediately has_warranties_yes has_warranties_no __typename } vehicles { id interiorColor exteriorColor modelID modelYear modelCode modelName modelPrice modelPowerkW modelMileage audiCode stockNumber trimName kvpsSyncId dealerName dealerRegion vehicleType warrantyType modelImageFromScs isAvailableNow vin bodyType saleOrderType vehicleInventoryType vehicleOrderStatus driveType gearType distanceFromUser __typename } __typename }}",  # noqa: B950
     }
+    # Setup the HTTPX client to be used for the many API calls throughout this router
+    http = AsyncHTTPClient(
+        base_url=audi_base_url, timeout_value=30.0, verify=verify_ssl
+    )
 
-    async with AsyncHTTPClient(
-        base_url=audi_base_url, timeout_value=60.0, verify=verify_ssl
-    ) as http:
-        g = await http.post(uri="/", headers=headers, post_data=inventory_post_data)
-
+    inv = await http.post(uri="/", headers=headers, post_data=inventory_post_data)
     try:
-        data = g.json()
+        inventory_data = inv.json()
     except ValueError:
         return error_response(
-            error_message=f"An error occurred with the Audi inventory service: {g.text}"
+            error_message=f"An error occurred with the Audi inventory service: {inv.text}"
         )
     try:
         # If the inventory request was successful, even if 0 vehicles are returned
         # the response will have the ['data'] dict, so validating that
-        data["data"]
-        return send_response(
-            response_data=data,
-            cache_control_age=3600,
-        )
+        inventory_data["data"]
+
     except KeyError:
-        print(data)
         error_message = "An error occurred with the Audi inventory service"
-        return error_response(error_message=error_message, error_data=data)
+        return error_response(error_message=error_message, error_data=inventory_data)
+
+    total_vehicle_count = inventory_data["data"]["getFilteredVehiclesForWormwood"][
+        "filterResults"
+    ]["totalCount"]
+
+    if total_vehicle_count <= amount_to_page_by:
+        return send_response(response_data=inventory_data)
+    else:
+        begin_index = amount_to_page_by
+        end_index = total_vehicle_count
+        step = 24
+
+        urls_to_fetch = []
+
+        for i in range(begin_index, end_index, step):
+            # Make a deep copy of the inventory_post_data dict to allow for value updates
+            # in this loop
+            tmp = copy.deepcopy(inventory_post_data)
+            tmp["variables"]["offset"] = i
+
+            # Add the post data to our URL list
+            urls_to_fetch.append(["/", headers, tmp])
+
+        remainder = await http.post(uri=urls_to_fetch)
+
+        # HTTP requests to the Audi API are complete, close the connection.
+        await http.close()
+
+        if type(remainder) is not list:
+            remainder = [remainder]
+
+        for api_result in remainder:
+            try:
+                result = api_result.json()
+                remainder_vehicles = result["data"]["getFilteredVehiclesForWormwood"][
+                    "vehicles"
+                ]
+                inventory_data["data"]["getFilteredVehiclesForWormwood"][
+                    "vehicles"
+                ].extend(remainder_vehicles)
+            except AttributeError:
+                inv["apiErrorResponse"] = True
+
+        return send_response(response_data=inventory_data, cache_control_age=3600)
 
 
 @router.get("/vin/audi")
